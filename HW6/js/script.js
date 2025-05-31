16b284be0cdde207a47ac799f5f0c7c6b1f9d3a1
Vlad106 <ruki.lolka50gp9@gmail.com>
2025-05-31 17:44:36 +0300

    Changed comments

--- HW6/js/script.js
+++ HW6/js/script.js
@@ -1,19 +1,19 @@
 'use strict'
 
-const name = 'Vladyslav'; //тип данных String - для текстовых строк, можно выводить текстовые данные и значения
-const age = 20; //тип данных Number - для чисел, можно выводить числовые данные , есть ограниченные значения (64-бит)
-let verified = true; //тип данных Boolean - для логических значений, для подтверждения действий , используется true или false
+const name = 'Vladyslav'; //data type String - for text lines, can show text data and text values
+const age = 20; //data type Number - for numbers, can show numbers values , has limited value (64-bit)
+let verified = true; //data type Boolean - for logical values, for approve acts , use true or false
 
-//  const registrationData = {  //тип данных Object - для объектов, для хранения данных которые занимают больший обьем памяти , используется для более динамических и сложных действий
+//  const registrationData = {  //data Object - for objects, for saving data that takes up more memory , use for more dynamic and complex acts
 //     month: 6,
 //     day: 10,
 //     year: 2004,
 // }
 // console.log(registrationData.day, registrationData.month, registrationData.year)
 
-let cardNumber = BigInt(12345678901234567890); //тип данных BigInt - для больших целых чисел, для хранения больших чисел , используется для работы с координатами и более сложными вычислениями
-let promoCode = null; //тип данных Null - для пустых значений, используется для дальнейшего заполнения
-let secondName; //тип данных Undefined - для неопределенных значений(пустое значение), зачастую принято не использовать
+let cardNumber = BigInt(12345678901234567890); //data type BigInt - for big numbers, for saving big numbers , use for working with coordinate and complex calculations
+let promoCode = null; //data type Null - for empty elements, use for further filling with an element
+let secondName; //data type Undefined - for undefined values ( empty element ) , often don't use
 let bonusAccount = 13;
 
 console.log(`Користувач ${name} , вік ${age} , верифікація: ${verified}.`)
