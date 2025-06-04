3d2feb5bb216e65b90c32dd7b2ca1fe69947bc5f
Vlad106 <ruki.lolka50gp9@gmail.com>
2025-06-04 19:04:07 +0300

    Did function with the conversation of hours to seconds

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let personalHours = +prompt(' Enter your hours');
+let minutes = personalHours * 60;
+let seconds = minutes * 60;
+
+const time = seconds;
+
+alert(`It's your time in seconds - ${time} `);
