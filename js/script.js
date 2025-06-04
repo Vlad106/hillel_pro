32ed6b260cc318f887178d66eb92d55bb8560d9a
Vlad106 <ruki.lolka50gp9@gmail.com>
2025-06-04 19:12:25 +0300

    Made function of calculating the average number

---
+++ js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let num1 = +prompt('First number');
+let num2 = +prompt('Second number');
+let num3 = +prompt('Third number');
+let avrNum = (num1+num2+num3)/3;
+
+alert(`Your average number is ${avrNum}`);
